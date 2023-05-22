7e29784b088aac58a7cc4d3729e322258bc50303
Taffarel Xavier <taffarel_deus@hotmail.com>
2023-05-22 17:58:23 -0300

    Deploy

--- assets/src/components/MyCarousel.vue
+++ assets/src/components/MyCarousel.vue
@@ -1,163 +1,90 @@
 <template>
   <div>
-    <div :id="id" class="relative" data-te-carousel-init data-te-carousel-slide>
-      <!--Carousel indicators-->
-      <div
-        class="absolute bottom-0 left-0 right-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0"
-        data-te-carousel-indicators
-      >
+    <div :id="id" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
         <button
           type="button"
           v-for="image in images"
           :key="image.id"
-          :data-te-target="'#' + id"
-          :data-te-slide-to="image.id"
-          :data-te-carousel-active="image.id == 0"
-          class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+          :data-bs-target="'#' + id"
+          :data-bs-slide-to="image.id"
+          :class="{ active: image.id == 0 }"
           aria-current="true"
-          aria-label="Slide 1"
+          :aria-label="'Slide' + (image.id + 1)"
         ></button>
+        <!-- <button
+          type="button"
+          :data-bs-target="'#' + id"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          :data-bs-target="'#' + id"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button> -->
       </div>
-
-      <!--Carousel items-->
-      <div
-        class="relative w-full overflow-hidden after:clear-both after:block after:content-['']"
-      >
-        <!--First item-->
+      <div class="carousel-inner">
         <div
-          class="relative float-left -mr-[100%] h-72 w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-          data-te-carousel-item
-          style="backface-visibility: hidden"
-          v-for="image in images"
+          v-for="(image, index) in images"
           :key="image.id"
-          :data-te-carousel-active="image.id == 0"
+          class="carousel-item"
+          :class="{ active: image.id == 0 }"
         >
-          <img :src="image.src" class="block w-full h-72" alt="..." />
+          <img :src="image.src" :alt="index" class="d-block w-100 h-96" />
         </div>
+        <!-- <div class="carousel-item">
+          <img src="assets/image002.jpg" class="d-block w-100 h-96" alt="..." />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="assets/background.png"
+            class="d-block w-100 h-96"
+            alt="..."
+          />
+        </div> -->
       </div>
-
-      <!--Carousel controls - prev item-->
       <button
-        class="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+        v-if="images.length > 1"
+        class="carousel-control-prev"
         type="button"
-        :data-te-target="'#' + id"
-        data-te-slide="prev"
+        :data-bs-target="'#' + id"
+        data-bs-slide="prev"
       >
-        <span class="inline-block h-8 w-8">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="h-6 w-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M15.75 19.5L8.25 12l7.5-7.5"
-            />
-          </svg>
-        </span>
-        <span
-          class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-          >Previous</span
-        >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Anterior</span>
       </button>
-      <!--Carousel controls - next item-->
       <button
-        class="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+       v-if="images.length > 1"
+        class="carousel-control-next"
         type="button"
-        :data-te-target="'#' + id"
-        data-te-slide="next"
+        :data-bs-target="'#' + id"
+        data-bs-slide="next"
       >
-        <span class="inline-block h-8 w-8">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="h-6 w-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M8.25 4.5l7.5 7.5-7.5 7.5"
-            />
-          </svg>
-        </span>
-        <span
-          class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-          >Next</span
-        >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Próximo</span>
       </button>
-    </div>
-    <div class="text-center">
-
-    <span v-if="images.length > 1" class="text-xs block text-center text-gray-500"
-      >Arraste para o lado ou clique no botões para passar</span
-    >
     </div>
   </div>
 </template>
 
 <script>
-import { Carousel, initTE } from "tw-elements";
-
 export default {
   props: {
-    id: String,
+    myid: String,
     images: Array,
   },
   data() {
     return {
-      currentIndex: 0,
-      carouselItems: null,
+      id: null,
     };
   },
   mounted() {
-    initTE({ Carousel });
+    this.id = this.myid;
   },
   methods: {},
 };
 </script>
 <style>
-.carousel {
-  overflow: hidden;
-  width: 100%;
-}
-
-.carousel-inner {
-  display: flex;
-  transition: transform 0.3s ease;
-}
-
-.carousel-item {
-  flex: 0 0 100%;
-}
-
-.carousel-item img {
-  width: 100%;
-}
-
-.carousel-control {
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  background-color: rgba(0, 0, 0, 0.5);
-  color: #fff;
-  border: none;
-  padding: 0.5rem;
-  font-size: 1.5rem;
-  cursor: pointer;
-}
-
-.carousel-control.prev {
-  left: 0;
-}
-
-.carousel-control.next {
-  right: 0;
-}
 </style>
